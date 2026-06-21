b5987b6943792432c6ddc1f50a77894a6edc8868
kaizokuv <luckjumanraj@gmail.com>
2026-06-21 20:02:56 +0800

    update: dashboard for nvim to fit da vibes :D

---
+++ installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo" ___  ___      ___ ___  ___"
+echo"|\  \|\  \    /  /|\  \|\  \"
+echo"\ \  \ \  \  /  / | \  \ \  \"
+echo" \ \  \ \  \/  / / \ \  \ \  \"
+echo"  \ \  \ \    / /   \ \  \ \  \"
+echo"   \ \__\ \__/ /     \ \__\ \__\"
+echo"    \|__|\|__|/       \|__|\|__|"
+
+echo"Welcome to the IVII Dots Installer"
+
+
